59fbeffe712ab94a0bbf47de24b5582cc7a85594
Edon Gashi <edon.gashi@uni-pr.edu>
2019-03-06 02:10:01 +0100

    caesar app

---
+++ public/apps/caesar-decryptor.js
@@ -0,0 +1,60 @@
+define(['react', '/public/js/nn-predict'], function (React, predict) {
+  const { useState, useEffect } = React
+
+  function useDebounce(value, delay) {
+    const [debouncedValue, setDebouncedValue] = useState(value)
+
+    useEffect(
+      () => {
+        const handler = setTimeout(() => {
+          setDebouncedValue(value)
+        }, delay)
+        return () => {
+          clearTimeout(handler)
+        }
+      },
+      [value]
+    )
+
+    return debouncedValue
+  }
+
+  function CaesarDecryptor() {
+    const [cipherText, setCipherText] = useState('')
+    const debouncedCipherText = useDebounce(cipherText, 1000)
+    const plainText = useMemo(() => predict(debouncedCipherText), [debouncedCipherText])
+    return <div>
+      <h3>Dekriptori i Kodit të Cezarit</h3>
+      <textarea
+        style={{
+          boxSizing: 'border-box',
+          width: '100%',
+          resize: 'vertical'
+        }}
+        value={cipherText}
+        onChange={e => setCipherText(e.target.value)} />
+      <hr />
+      {debouncedCipherText && <table>
+        <tr>
+          <th>Plaintext</th>
+          <th>Probabiliteti</th>
+        </tr>
+        <tr>
+          <td>{plainText[0].text}</td>
+          <td>{plainText[0].confidence}</td>
+        </tr>
+        <tr>
+          <td>{plainText[1].text}</td>
+          <td>{plainText[1].confidence}</td>
+        </tr>
+        <tr>
+          <td>{plainText[2].text}</td>
+          <td>{plainText[2].confidence}</td>
+        </tr>
+      </table>}
+    </div>
+  }
+
+  CaesarDecryptor.title = 'Dekriptori i Kodit të Cezarit'
+  return CaesarDecryptor
+})
